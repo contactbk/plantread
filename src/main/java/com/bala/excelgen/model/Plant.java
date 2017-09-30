a1a276d330657903c7421442f14befea6ee9ca42
Balaji Kesavan <contactbalajikesavan@gmail.com>
2017-10-01 07:51:45 +0800

    plant update
    update plant

---
+++ src/main/java/com/bala/excelgen/model/Plant.java
@@ -0,0 +1,64 @@
+package com.bala.excelgen.model;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="t_plant")
+@NamedQuery(name="Plant.findAll",query="SELECT c FROM Plant c")
+public class Plant implements Serializable{
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="ID", updatable = false)
+  private Long id;
+@Column(name="NAME")
+  private String name;
+@Column(name="TYPE")
+  private String type;
+@Column(name="CATEGORY")
+  private String category;
+@Column(name="PRICE")
+  private Long price;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public Long getPrice() {
+    return price;
+  }
+
+  public void setPrice(Long price) {
+    this.price = price;
+  }
+}
--- src/main/resources/db-scripts/INSERT_PLANT.sql
+++ src/main/resources/db-scripts/INSERT_PLANT.sql
@@ -1,4 +1,4 @@
-INSERT INTO plant(id,name,type,category,price)
+INSERT INTO t_plant(id,name,type,category,price)
 VALUES(002,'bostonfern','tropical','indoor',20);
 
-SELECT * from plant;
+SELECT * from t_plant;
