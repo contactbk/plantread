2089c5c5957554cc15ef909912f2d5c99d2d232c
Balaji Kesavan <contactbalajikesavan@gmail.com>
2017-09-30 20:08:15 +0800

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/bala/excelgen/ExcelGenApplication.java
@@ -0,0 +1,13 @@
+package com.bala.excelgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelGenApplication.class, args);
+		System.out.println("test ");
+	}
+}
